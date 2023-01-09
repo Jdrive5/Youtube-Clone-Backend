461dd09cd4d7fd74fdf07545a7a7e8d41cd556ca
Monica Banda <monicabanda@Monicas-Air.lan>
2023-01-09 13:27:39 -0700

    Completed Backend

--- backend/comment/serializers.py
+++ backend/comment/serializers.py
@@ -6,5 +6,5 @@ from .models import Comment
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ['user' 'video_id', 'text', 'likes', 'dislikes']
+        fields = ['user', 'video_id', 'text', 'likes', 'dislikes']
         depth = 1
