4addf0406975dc405b427b1a91207e0c236f0074
Monica Banda <monicabanda@Monicas-Air.lan>
2023-01-19 11:29:16 -0700

    Added Homepage for Frontend

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -3,39 +3,51 @@ import { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
 
 import axios from "axios";
+import { KEY } from "../../localKey"
+
 
 const HomePage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   //TODO: Add an AddCars Page to add a car for a logged in user's garage
   const [user, token] = useAuth();
-  const [cars, setCars] = useState([]);
+  const [videos, setVideos] = useState([]);
+  const [search,setSearch] = useState("");
 
   useEffect(() => {
-    const fetchCars = async () => {
-      try {
-        let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
-          headers: {
-            Authorization: "Bearer " + token,
-          },
-        });
-        setCars(response.data);
-      } catch (error) {
-        console.log(error.response.data);
-      }
-    };
-    fetchCars();
+    getVideos();
   }, [token]);
+
+
+  async function getVideos() {
+    try {
+      let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=NBAHighlights&key=${KEY}&part=snippet&type=video&maxResults=4`, (videos));
+      
+      console.log(response.data.items);
+      setVideos(response.data.items);
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  }
+
+    function handleSubmit(event) {
+      event.preventDefault();
+      getVideos()
+    }
+   
   return (
-    <div className="container">
-      <h1>Home Page for {user.username}!</h1>
-      {cars &&
-        cars.map((car) => (
-          <p key={car.id}>
-            {car.year} {car.model} {car.make}
-          </p>
-        ))}
-    </div>
+    <>
+      <div className="container">
+        <h1>Home Page for {user.username}!</h1>
+      </div>
+      <div className="search-bar">
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={search} onChange={(event) => setSearch(event.target.value)}/>
+        <button className="button" type="submit">Search</button>
+        </form>
+
+      </div>
+    </>
   );
 };
 
