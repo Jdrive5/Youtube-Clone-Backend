5d5027fa821381cc2ddafba3b7e1154e79ce1a67
Monica Banda <monicabanda@Monicas-Air.lan>
2023-01-19 17:21:05 -0700

    Added SearchBar Component to Components Folder

---
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import axios from 'axios';
+
+
+const SearchBar = (props) => {
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
+    return (
+        <div>
+            <div className='searchbar'>
+                <div className='searchtext'> Search Videos</div>
+                <div className='searchform'>
+                    <input
+                    type="text"
+                    value={props.search}
+                    onChange={(event) => props.setSearch(event.target.value)}
+                    ></input>
+                    <div>
+                        <button onClick={() => props.getVideos()}>Search Here</button>
+                        <br></br>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+
+export default SearchBar
