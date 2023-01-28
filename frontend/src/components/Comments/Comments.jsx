ae203c36f446a7d188ba212c50f5923238167030
Monica Banda <monicabanda@Monicas-Air.lan>
2023-01-27 23:47:54 -0700

    Fixed Video Page to Show Youtube Video

--- frontend/src/components/Comments/Comments.jsx
+++ frontend/src/components/Comments/Comments.jsx
@@ -7,7 +7,7 @@ const Comments = ({videoId}) => {
 
     const fetchComments = async (videoId) => {
         try {
-            let response = await axios.get(`http://127.0.0.1:8000/api/comments/?video_id=${videoID}`);
+            let response = await axios.get(`http://127.0.0.1:8000/api/comments/?video_id=${videoId}`);
 
             setComments(response.data)
         }
--- frontend/src/pages/VideoPage/VideoPage.js
+++ frontend/src/pages/VideoPage/VideoPage.js
@@ -2,64 +2,32 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { KEY } from '../../localKey';
-import useAuth from '../../hooks/useAuth';
 import VideoComments from '../../components/VideoComments/VideoComments';
 import "./VideoPage.css";
+import VideoMapper from '../../components/VideoMapper/VideoMapper';
 
 
-const VideoPage = (props) => {
-    const [video, setVideo] = useState([]);
-    const [relatedVideos, setRelatedVideos] = useState([]);
-    const { videoId } = useParams();
+const VideoPage = () => {
+    const [videos, setVideos] = useState([]);
 
-    useEffect(() => {
-        let mounted = true;
-        if (mounted) {
-            getVideoInfo();
-            getRelatedVideos();
-        }
-        return () => (mounted = false);
-    },  [videoId]);
-
-    async function getVideoInfo(){
-        const response =  await axios.get(
-            `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${KEY}`
-        );
-        setVideo(response["data"]["items"][0]);
+    const getVids =  async()=>{
+        await axios.get(`https://www.googleapis.com/youtube/v3/search?relatedToVideoId=DldQhe4azr4&type=video&maxResults=4&key=${KEY}&part=snippet`)
+                    .then(res => {setVideos(res.data.items)})
     } 
     
 
 
-    async function getRelatedVideos() {
-        const response = await axios.get(
-            `https://www.googleapis.com/youtube/v3/search?relatedToVideoId=${videoId}&type=video&key=${KEY}&part=snippet`
-        );
-        setRelatedVideos(response["data"]["items"]);
-    }
-
-
 return (
         <div>
-            <div>
-                <div className='videocontainer'>
-                    <div>
-                        <div>
-                            <br></br>
-                        </div>
-
-                        <iframe
-                            id="ytplayer"
-                            type="text/html"
-                            width="640"
-                            height="360"
-                            src={`https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1&origin=http://example.com`}
-                            frameBorder="0"
-                        ></iframe>
-                        <VideoComments videoId={video.id} />
-                        <br></br>
-                    </div>
-                </div>
-            </div>
+            <iframe
+                title="main-vid-player"
+                id="player" 
+                type="text/html" 
+                width="640" 
+                height="390"
+                src={`http://www.youtube.com/embed/iUbuI3tT9KU?enablejsapi=1&origin=http://example.com`}
+                frameborder="0"
+            ></iframe>
         </div>
     );
 
